aacf44e12dfad96d8dde6ef3f41b986e3826fbd6
Jérémy <jeremycloarec@msn.com>
2023-09-25 17:03:11 +0200

    Point de terminaison

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 
 // On récupère le paquet express dans notre code. On va chercher la dépendance express dans notre module
+const { log } = require('console')
 const express = require('express')
 
 // On crée une instance de l'application exprss. Petit serveur web.
@@ -18,8 +19,28 @@ const port = 3000
 //  - req : permet de recuperer un objet request correspondant à la requête recu en entrée
 // -res : correspond à la réponse, l'objet qu'on doit renvoyer depuis express à notre client
 // On utilise la methode send de l'objet response pour afficher notre message
-app.get('/', (req, res) => res.send('Hello, Express !'))
+app.get('/', (req, res) => res.send('Hello again, Express !'))
 
 // On démarre notre appli sur le port 3000 et on affiche message de confirmation grace à la methode listen
 
+// On ajoute un nouveau chemin
+// Avec Express on peut récupérer des paramètres de l'URL directement depuis nos points de terminaison. 
+
+// Il faut récupérer l'identifiant contenu dans l'url
+
+// Express recupere la valeur du param et le transmettre dans le point de terminaison via l'objet req
+
+app.get('/api/pokemon/:id/:name',(req, res) => { 
+
+    // On extrait l'identifaint contenu dans l'URL
+    const id = req.params.id
+    const name = req.params.id
+
+    res.send(`Vous aves demandé le pokemon n° ${id} dont le nom est ${name}`)
+})
+
+
+
 app.listen(port, () => console.log(`Notre application est démarrée sur http://localhost:${port}`))
+
+
