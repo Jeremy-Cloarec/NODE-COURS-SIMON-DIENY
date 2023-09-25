a46473e49680231051691adb2da655c06531fd60
Jérémy <jeremycloarec@msn.com>
2023-09-25 15:59:50 +0200

    Installation node_modules

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-console.log("Hello, Node !")
+// console.log("Coucou, Node !")
 
-//On récupère le paquet express dans notre code. require = indique au js de chercher la dépendance express dans notre dossier. 
-const express = require('express') 
 
-//On crée une instance d'une application express grâce à la méthode du même nom. Il s'agit du petit serveur web sur laquelle va fonctionner notre API rest.
+// On récupère le paquet express dans notre code. On va chercher la dépendance express dans notre module
+const express = require('express')
+
+// On crée une instance de l'application exprss. Petit serveur web.
 const app = express()
 
-//On définit une constante nommé port avec la valeur 3000. Port sr laquelle on va démarrer l'api
+// Port sur laquelle on démarre notre API rest. Petit s
 const port = 3000
 
-//On définit notre premier point de terminaison (handpoint).
-//Méthode : GET. Prend en paramètre deux élé. 1)Chemin de la requête qui va permette de traiter le point de terminaison. 2) Fonction dont le rôle est de fournir une répoonse au client lorsque le point de terminaison est appelé. Cette fonction a elle -même deux arguments en entrée. 1 :req : permet de récupérer un objet request correspondant à la requête reçue en entrée par notre point de terminaison. 2:correspond à la réponse : l'objet que l'on doit renvoyer  à notre client.  
-app.get('/', (req,res) => res.send("Hello, Express !" ))
+// Point de terminaison.
+// Methode de la requête : 2 élé : 
 
-//On démarre l'PI rest sur le port 3000. 
-app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
+//1  Le chemin de la requête 
+//2 Réponse au client qui a elle même deux arguments en entrée : 
+//  - req : permet de recuperer un objet request correspondant à la requête recu en entrée
+// -res : correspond à la réponse, l'objet qu'on doit renvoyer depuis express à notre client
+// On utilise la methode send de l'objet response pour afficher notre message
+app.get('/', (req, res) => res.send('Hello, Express !'))
+
+// On démarre notre appli sur le port 3000 et on affiche message de confirmation grace à la methode listen
+
+app.listen(port, () => console.log(`Notre application est démarrée sur http://localhost:${port}`))
